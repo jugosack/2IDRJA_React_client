f546c42c320eeea7ccb5e91f42618a5d814f712c
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2023-12-24 22:05:38 +0100

    creating components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./components/icons/logo.svg";
 import "./App.css";
+import Navbar from "./components/ui/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  <>
+    <div>
+      <div>
+        <h1>home</h1>
+        <h1>about</h1>
+        <h1>contact us</h1>
+      </div>
+    </div>
+  </>;
+};
+
+export default Navbar;
