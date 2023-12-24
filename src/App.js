d1f3fe87b0ac418e793353fe26140c104feec92a
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2023-12-24 22:13:43 +0100

    adding components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import "./App.css";
 import Navbar from "./components/ui/Navbar";
 
 function App() {
+  const header = () => {
+    return (
+      <div>
+        <h1>home</h1>
+        <h1>home</h1>
+        <h1>home</h1>
+      </div>
+    );
+  };
   return (
     <div className="App">
       <Navbar />
+      <header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
