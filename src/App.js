3718369128a476a53ae65c1781c5e06cfef75eba
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-01-10 15:06:11 +0100

    impporting the wallpaper for background

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Navbar from './components/ui/Navbar';
 function App() {
   return (
     <div className="App">
-      <header />
+      <div className='background'>
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </div>
       <Navbar />
+      
 
+      </div>
     </div>
   );
 }
