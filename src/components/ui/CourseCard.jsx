458a89c3af7a180c76413f2930d07e7f3af4799e
Marko Stojanovikj <stojanovikmarko7916@gmail.com>
2024-11-05 10:17:45 +0100

    fixed props validation

--- src/components/ui/CourseCard.jsx
+++ src/components/ui/CourseCard.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './CourseCard.css';
 
-const CourseCard = (props) => (
+const CourseCard = ({
+  image, title, duration, discount, price, places,
+}) => (
   <>
     <div className="c-card d-flex flex-column justify-content-center align-items-center m-3 shadow-lg rounded">
       <img
         className="d-flex image-fluid rounded-top course-image shadow-lg"
-        src={props.image}
+        src={image}
         alt="course"
       />
       <div className="d-flex flex-row p-3 mt-3 rounded justify-content-center align-items-center w-100">
-        <p className="cource-paragraph fs-2">{props.title}</p>
+        <p className="cource-paragraph fs-2">{title}</p>
       </div>
       <div className="d-flex flex-row  rounded justify-content-center align-items-center w-100">
-        <p className="cource-paragraph-date m-0">{props.duration}</p>
+        <p className="cource-paragraph-date m-0">{duration}</p>
       </div>
       <div className="d-flex flex-row  rounded justify-content-center align-items-center w-100">
         <p className="fs-2 text-secondary fw-bold me-4">
-          <s>{props.discount}</s>
+          <s>{discount}</s>
         </p>
-        <p className="fs-2 text-dark fw-bold">{props.price}</p>
+        <p className="fs-2 text-dark fw-bold">{price}</p>
       </div>
       <div className="d-flex flex-row  rounded justify-content-center align-items-center w-100">
-        <p className="places-left fs-4 text-danger">{props.places}</p>
+        <p className="places-left fs-4 text-danger">{places}</p>
       </div>
 
       <div className="d-flex flex-row px-5 pb-4 pt-3 mb-3 rounded w-100">
@@ -33,4 +36,14 @@ const CourseCard = (props) => (
     </div>
   </>
 );
+
+CourseCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired,
+  discount: PropTypes.string,
+  price: PropTypes.string.isRequired,
+  places: PropTypes.string.isRequired,
+};
+
 export default CourseCard;
--- src/components/ui/CourseEnrollmentCard.jsx
+++ src/components/ui/CourseEnrollmentCard.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './CourseEnrollmentCard.css';
 
-const CourseEnrollmentCard = (props) => (
+const CourseEnrollmentCard = ({
+  image, title, duration, price, places,
+}) => (
   <>
     <>
       <div className="enrollment-card d-flex justify-content-center align-items-cente m-3">
         <div className="card-background">
-          <img className="card-wallpaper rounded-top" src={props.image} alt="" />
+          <img className="card-wallpaper rounded-top" src={image} alt="" />
           <div className="card-body justify-content-center align-items-center pt-4">
             <h6 className="card-title text-center text-secondary fs-3 fw-bold pb-4">
-              {props.title}
+              {title}
             </h6>
-            <p className="card-text text-dark text-center p-2">{props.duration}</p>
-            <p className="card-text text-dark fs-3 fw-bold text-center p-2">{props.price}</p>
-            <p className="card-text text-dark text-center pb-3">{props.places}</p>
+            <p className="card-text text-dark text-center p-2">{duration}</p>
+            <p className="card-text text-dark fs-3 fw-bold text-center p-2">{price}</p>
+            <p className="card-text text-dark text-center pb-3">{places}</p>
             <div className="pt-5 d-flex justify-content-center align-items-center w-100">
               <button type="button" className="btn text-dark btn-custom w-75">
                 Enroll now
@@ -25,5 +29,13 @@ const CourseEnrollmentCard = (props) => (
     </>
   </>
 );
+
+CourseEnrollmentCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  places: PropTypes.string.isRequired,
+};
 
 export default CourseEnrollmentCard;
